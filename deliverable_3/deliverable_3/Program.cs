1174c329eca890f4b31c44126e72009f4f79b5d9
hai <101365274+hloub@users.noreply.github.com>
2023-01-31 11:20:10 -0500

    Add files via upload

---
+++ deliverable_3/deliverable_3/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace deliverable_3;
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        
+            Console.Write("Enter an integer between 1 and 100: ");
+        try
+        {
+            int input = int.Parse(Console.ReadLine());
+        
+
+            Console.Write("Specify the series type: even or odd ");
+            string series = Console.ReadLine();
+
+            if (series == "odd")
+            {
+
+                Console.Write("You have selected " + series + " series. The numbers between 0 and " + input + " are: ");
+
+                for (int i = 1; i < input; i = i + 2)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            if (series == "even")
+            {
+                Console.Write("You have selected " + series + " series. The numbers between 0 and " + input + " are: ");
+
+                for (int i = 0; i <= input; i = i + 2)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+        catch
+        {
+            Console.Write("Enter a numeric value!");
+        }
+    }
+}
+
